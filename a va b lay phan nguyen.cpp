689a90b5172faa676d372b67e226d1a725996f4b
trunggkiennnn <93917219+trunggkiennnn@users.noreply.github.com>
2021-11-09 16:23:16 +0700

    Add files via upload

---
+++ a va b lay phan nguyen.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a, b;
+	printf("\ nhap vao so nguyen a: ");
+	scanf("%d", &a);
+	printf("\n Nhap vao so nguyen b: ");
+	scanf("%d", &b);
+	printf("\n Chia  lay phan nguyen cua a chia b = %d ", a/b);
+	getch();
+
+}
+
